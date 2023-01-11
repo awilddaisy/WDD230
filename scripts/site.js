5507d2a3365073af909ae44163d1bb5d823da683
awilddaiy <mue19002@byui.edu>
2023-01-11 13:12:01 -0700

    w01 changes

---
+++ scripts/site.js
@@ -0,0 +1,31 @@
+//get current date 
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + "," + monthName + " " + d.getDate() + "," + d.getFullYear();
+
+document.getElementById("lastmodified").textContent = fulldate;
